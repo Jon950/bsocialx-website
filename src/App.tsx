09eb1d0101d2f3b74bcf15c7b2ffecfc908082fe
BusinessOperatingSystem-IT <86786333+BusinessOperatingSystem-IT@users.noreply.github.com>
2021-10-01 16:39:41 -0400

    working on hero

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+// React
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+// CSS
+import './styleSheet/App.css';
+
+// Pages
+import Home from "./pages/Home"
+
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+// React
+import React from 'react';
+
+// CSS
+import '../styleSheet/hero.css';
+
+function Hero() {
+  return (
+   <section className="hero">
+       <div className="textBox">
+           <h1>BsocialX</h1>
+           <p>Be the First to win!</p>
+       </div>
+   </section>
+  );
+}
+
+export default Hero;
