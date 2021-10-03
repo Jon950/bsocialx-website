43ea55264055d81718c574d9de539bb835077e52
BusinessOperatingSystem-IT <86786333+BusinessOperatingSystem-IT@users.noreply.github.com>
2021-10-03 16:51:27 -0400

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,16 +5,16 @@ import React from 'react';
 import '../styleSheet/footer.css';
 
 function Footer() {
-    const submitQuestion = (event:any) => {
-        event.preventDefault();
-    }
+    // const submitQuestion = (event:any) => {
+    //     event.preventDefault();
+    // }
   return (
    <footer>
        <h2>Just SignUp Now!</h2>
        <a href="https://bsocialxapp.netlify.app" className="smallaTagBtn" >SignUp with BsocialX</a>
 
        <h4>Have any questions?</h4>
-       <form name="contact" method="POST" data-netlify="true" onSubmit={submitQuestion}>
+       <form name="contact" method="POST" data-netlify="true">
            <input type="text" name="name" placeholder="Your name..." required/>
            <button type="submit" >Send</button>
 
