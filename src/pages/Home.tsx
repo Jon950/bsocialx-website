ce85ca9e6bdf2530f0b1e502f92d4e8c1c85b974
BusinessOperatingSystem-IT <86786333+BusinessOperatingSystem-IT@users.noreply.github.com>
2021-10-03 20:00:42 -0400

    working on added Swiper

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,12 @@
 // React
 import React from 'react';
 
+// Swiper
+import {Swiper, SwiperSlide} from 'swiper/react';
+// import {Navigation, Pagination, Scrollbar, A11y} from "swiper";
+import 'swiper/swiper-bundle.css';
+
+
 // Components
 import Header from "../components/Header";
 import Hero from "../components/Hero";
@@ -8,13 +14,42 @@ import SmallAbout from "../components/SmallAbout";
 import Feachers from "../components/Feachers";
 import Footer from "../components/Footer";
 
+// CSS
+import "../styleSheet/home.css"
+
 function Home() {
+  const items = [
+    {id: "1", title: "e"},
+    {id: "2", title: "D"},
+    {id: "3", title: "g"},
+    {id: "4", title: "s"},
+    {id: "5", title: "e"},
+    {id: "6", title: "D"},
+    {id: "7", title: "g"},
+    {id: "8", title: "s"}
+  ]
   return (
    <>
    <Header />
    <Hero />
    <main>
        <SmallAbout/>
+       <hr />
+       <Swiper 
+       tag="section" 
+       wrapperTag="ul" 
+       className="carousel" 
+      //  modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={3}
+      // navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}>
+         {items.map((item) => 
+           <SwiperSlide tag="li" key={item.id} className="card">{item.title}</SwiperSlide>
+         )}
+      ...
+    </Swiper>
        <hr />
        <Feachers />
    </main>
