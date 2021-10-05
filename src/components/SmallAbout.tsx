822c24b90bb59da1638860ca62c16c1c33e13371
BusinessOperatingSystem-IT <86786333+BusinessOperatingSystem-IT@users.noreply.github.com>
2021-10-05 10:22:08 -0400

    looks good

--- src/components/SmallAbout.tsx
+++ src/components/SmallAbout.tsx
@@ -20,8 +20,8 @@ function SmallAbout() {
   const items = [
     {id: "1", title: "Learn about BsocialX", details: "A simplified way to rate your friends and find new ones.", url: AppIcon, altTag: "Brookdale mascot", className: "appIcon"},
     {id: "2", title: "Sign in with Google!", details: "Use your Google account to sign in to the BsocialX app. To sign in just click Sign in with Google. If you don't have an account with BsocialX, just click Sign in with Google it will make one for you. You won't have to remember individual usernames and passwords for each account.", url: PhoneSignInView, altTag: "BsocialX sign in page", className: "phoneView"},
-    {id: "3", title: "Find everthing on your home page", details: "", url: PhoneHomeView, altTag: "BsocialX home page", className: "phoneView"},
-    {id: "4", title: "Find all your friends and new one", details: "", url: PhoneFriendView, altTag: "BsocialX friend finder page", className: "phoneView"}
+    {id: "3", title: "Find everthing on your home page", details: "BsocialX app homepage is the door to a world of social interaction, and a good start is half the battle.", url: PhoneHomeView, altTag: "BsocialX home page", className: "phoneView"},
+    {id: "4", title: "Find all your friends and new one", details: "Small acts of kindness can make a big impact. Find people and make that difference.", url: PhoneFriendView, altTag: "BsocialX friend finder page", className: "phoneView"}
   ];
 
   return (
@@ -47,7 +47,7 @@ function SmallAbout() {
             <SwiperSlide tag="li" key={item.id} className={item.className}>
 
               <div className="textBox">
-                <h4>{item.title}</h4>
+                <h3>{item.title}</h3>
                 <p>{item.details}</p>
               </div>
               <img src={item.url} alt={item.altTag}  ></img>
